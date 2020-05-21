2ba0b94261cd112207b72af89c569b094bf6300e
Renz Serilo <serilorenz112792@gmail.com>
2020-05-21 10:57:36 +0800

    ey

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 
 app.use(express.json())
+app.use(cors());
 
 
 
